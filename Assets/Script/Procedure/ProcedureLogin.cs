6e5a4cd79d72648bc463fd2bb16673aa896a50b7
longshilin <583297550@qq.com>
2020-08-08 22:06:04 +0800

    [*]增加登录界面

---
+++ Assets/Script/Procedure/ProcedureLogin.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HT.Framework;
+using DG.Tweening;
+
+namespace com.longshilin.TankWar
+{
+    /// <summary>
+    /// 登录流程
+    /// </summary>
+    public class ProcedureLogin : ProcedureBase
+    {
+        /// <summary>
+        /// 流程初始化
+        /// </summary>
+        public override void OnInit()
+        {
+        }
+
+        /// <summary>
+        /// 进入流程
+        /// </summary>
+        /// <param name="lastProcedure">上一个离开的流程</param>
+        public override void OnEnter(ProcedureBase lastProcedure)
+        {
+            Main.m_UI.OpenResidentUI<UILogin>();
+        }
+
+        /// <summary>
+        /// 离开流程
+        /// </summary>
+        /// <param name="nextProcedure">下一个进入的流程</param>
+        public override void OnLeave(ProcedureBase nextProcedure)
+        {
+            Main.m_UI.CloseUI<UILogin>();
+        }
+
+        /// <summary>
+        /// 流程帧刷新
+        /// </summary>
+        public override void OnUpdate()
+        {
+        }
+
+        /// <summary>
+        /// 流程帧刷新（秒）
+        /// </summary>
+        public override void OnUpdateSecond()
+        {
+        }
+    }
+}
