0cdb6dfd8d0cc30553ccbf31e87ad842724827bb
longshilin <583297550@qq.com>
2020-08-09 00:32:16 +0800

    [*]提交项目代码

--- Assets/Script/Procedure/ProcedureReady.cs
+++ Assets/Script/Procedure/ProcedureReady.cs
@@ -4,49 +4,48 @@ using UnityEngine;
 using HT.Framework;
 using DG.Tweening;
 
-namespace com.longshilin.TankWar
+
+/// <summary>
+/// 准备流程
+/// </summary>
+public class ProcedureReady : ProcedureBase
 {
     /// <summary>
-    /// 准备流程
+    /// 流程初始化
     /// </summary>
-    public class ProcedureReady : ProcedureBase
+    public override void OnInit()
     {
-        /// <summary>
-        /// 流程初始化
-        /// </summary>
-        public override void OnInit()
-        {
-        }
+    }
 
-        /// <summary>
-        /// 进入流程
-        /// </summary>
-        /// <param name="lastProcedure">上一个离开的流程</param>
-        public override void OnEnter(ProcedureBase lastProcedure)
-        {
-            // Main.m_UI.OpenResidentUI<UIReady>();
-        }
+    /// <summary>
+    /// 进入流程
+    /// </summary>
+    /// <param name="lastProcedure">上一个离开的流程</param>
+    public override void OnEnter(ProcedureBase lastProcedure)
+    {
+        Main.m_UI.OpenResidentUI<UIReady>();
+    }
 
-        /// <summary>
-        /// 离开流程
-        /// </summary>
-        /// <param name="nextProcedure">下一个进入的流程</param>
-        public override void OnLeave(ProcedureBase nextProcedure)
-        {
-        }
+    /// <summary>
+    /// 离开流程
+    /// </summary>
+    /// <param name="nextProcedure">下一个进入的流程</param>
+    public override void OnLeave(ProcedureBase nextProcedure)
+    {
+        Main.m_UI.CloseUI<UIReady>();
+    }
 
-        /// <summary>
-        /// 流程帧刷新
-        /// </summary>
-        public override void OnUpdate()
-        {
-        }
+    /// <summary>
+    /// 流程帧刷新
+    /// </summary>
+    public override void OnUpdate()
+    {
+    }
 
-        /// <summary>
-        /// 流程帧刷新（秒）
-        /// </summary>
-        public override void OnUpdateSecond()
-        {
-        }
+    /// <summary>
+    /// 流程帧刷新（秒）
+    /// </summary>
+    public override void OnUpdateSecond()
+    {
     }
 }
--- Assets/Script/UI/UILogin.cs
+++ Assets/Script/UI/UILogin.cs
@@ -4,73 +4,71 @@ using UnityEngine;
 using HT.Framework;
 using DG.Tweening;
 
-namespace com.longshilin.TankWar
+
+/// <summary>
+/// 新建UI逻辑类
+/// </summary>
+[UIResource("ui", "Assets/Prefab/UI/UILogin.prefab", "null")]
+public class UILogin : UILogicResident
 {
     /// <summary>
-    /// 新建UI逻辑类
+    /// 初始化
     /// </summary>
-    [UIResource("ui", "Assets/Prefab/UI/UILogin.prefab", "null")]
-    public class UILogin : UILogicResident
+    public override void OnInit()
     {
-        /// <summary>
-        /// 初始化
-        /// </summary>
-        public override void OnInit()
-        {
-            base.OnInit();
+        base.OnInit();
 
-            UIEntity.FindChildren("Button_Play").rectTransform().AddEventListener(OnPlay);
-            UIEntity.FindChildren("Button_Quit").rectTransform().AddEventListener(OnQuit);
-        }
+        UIEntity.FindChildren("Button_Play").rectTransform().AddEventListener(OnPlay);
+        UIEntity.FindChildren("Button_Quit").rectTransform().AddEventListener(OnQuit);
+    }
 
-        /// <summary>
-        /// 打开UI
-        /// </summary>
-        public override void OnOpen(params object[] args)
-        {
-            base.OnOpen(args);
-        }
+    /// <summary>
+    /// 打开UI
+    /// </summary>
+    public override void OnOpen(params object[] args)
+    {
+        base.OnOpen(args);
+    }
 
-        /// <summary>
-        /// 置顶UI
-        /// </summary>
-        public override void OnPlaceTop()
-        {
-            base.OnPlaceTop();
-        }
+    /// <summary>
+    /// 置顶UI
+    /// </summary>
+    public override void OnPlaceTop()
+    {
+        base.OnPlaceTop();
+    }
 
-        /// <summary>
-        /// 关闭UI
-        /// </summary>
-        public override void OnClose()
-        {
-            base.OnClose();
-        }
+    /// <summary>
+    /// 关闭UI
+    /// </summary>
+    public override void OnClose()
+    {
+        base.OnClose();
+    }
 
-        /// <summary>
-        /// 销毁UI
-        /// </summary>
-        public override void OnDestroy()
-        {
-            base.OnDestroy();
-        }
+    /// <summary>
+    /// 销毁UI
+    /// </summary>
+    public override void OnDestroy()
+    {
+        base.OnDestroy();
+    }
 
-        /// <summary>
-        /// UI逻辑刷新
-        /// </summary>
-        public override void OnUpdate()
-        {
-            base.OnUpdate();
-        }
+    /// <summary>
+    /// UI逻辑刷新
+    /// </summary>
+    public override void OnUpdate()
+    {
+        base.OnUpdate();
+    }
 
-        private void OnPlay()
-        {
-            Main.m_Procedure.SwitchProcedure<ProcedureReady>();
-        }
+    private void OnPlay()
+    {
+        Main.m_Procedure.SwitchProcedure<ProcedureReady>();
+    }
 
-        private void OnQuit()
-        {
-            Application.Quit();
-        }
+    private void OnQuit()
+    {
+        Application.Quit();
     }
 }
